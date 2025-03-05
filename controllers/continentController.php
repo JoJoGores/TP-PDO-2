cdaafd89681903e82b998d595759c524eeb45ea5
JoJoGores <jovangores@gmail.com>
2025-03-05 20:24:06 +0100

    maison 05/03/25

--- controllers/continentController.php
+++ controllers/continentController.php
@@ -3,49 +3,47 @@ $action=$_GET['action'];
 switch($action){
     case 'list':
         $lesContinents=Continent::findAll();
-        include('vues/listeContinents.php');
+        include('vues/continent/listeContinent.php');
         break;
-    case 'add':
+    case 'add' :
         $mode="Ajouter";
-        include('vues/formContinent.php');
+        include('vues/continent/formContinent.php');
         break;
     case 'update':
         $mode="Modifier";
         $continent=Continent::findById($_GET['num']);
-        include('vues/formContinent.php');
+        include('vues/continent/formContinent.php');
         break;
-    case 'delete':
+    case 'delete' :
         $continent=Continent::findById($_GET['num']);
         $nb=Continent::delete($continent);
         if($nb==1){
             $_SESSION['message']=["success"=>"Le continent a bien été supprimé"];
         }else{
-            $_SESSION['message']=["danger"=>"Le continent n'a pas été supprimé"];
+            $_SESSION['message']=["success"=>"Le continent n'a pas été supprimé"];
         }
-        header('location: index.php?uc=continents&action=list');
+        header('location:index.php?uc=continent&action=list');
+        exit();
         break;
-    case 'valideForm':
-        $continent= new Continent();
+    case 'valideForm' :
         if(empty($_POST['num'])){
             $continent->setLibelle($_POST['libelle']);
             $nb=Continent::add($continent);
-            $message = "ajoutée";  
+            $message = "ajouté";
         }else{
-            $continent->setLibelle($_POST['libelle']);
             $continent->setNum($_POST['num']);
+            $continent->setLibelle($_POST['libelle']);
             $nb=Continent::update($continent);
-            $message = "modifiée";  
+            $message = "modifié";
         }
 
         if($nb==1){
             $_SESSION['message']=["success"=>"Le continent a bien été $message"];
         }else{
-            $_SESSION['message']=["danger"=>"Le continent n'a pas été $message"];
+            $_SESSION['message']=["success"=>"Le continent n'a pas été $message"];
         }
-        header('location: index.php?uc=continents&action=list');
+        header('location:index.php?uc=continent&action=list');
+        exit();
         break;
 }
-
-
-
 ?>
--- controllers/nationaliteController.php
+++ controllers/nationaliteController.php
@@ -1,51 +1,60 @@
 <?php
 $action=$_GET['action'];
 switch($action){
-    case 'list':
-        $lesNationalites=Nationalite::findAll();
-        include('vues/listeNationalites.php');
-        break;
-    case 'add':
-        $mode="Ajouter";
-        include('vues/formNationalite.php');
-        break;
-    case 'update':
-        $mode="Modifier";
-        $nationalite=Nationalite::findById($_GET['num']);
-        include('vues/formNationalite.php');
-        break;
-    case 'delete':
-        $nationalite=Nationalite::findById($_GET['num']);
-        $nb=Nationalite::delete($nationalite);
-        if($nb==1){
-            $_SESSION['message']=["success"=>"La nationalite a bien été supprimé"];
-        }else{
-            $_SESSION['message']=["danger"=>"La nationalite n'a pas été supprimé"];
-        }
-        header('location: index.php?uc=nationalites&action=list');
-        break;
-    case 'valideForm':
-        $nationalite= new Nationalite();
-        if(empty($_POST['num'])){
-            $nationalite->setLibelle($_POST['libelle']);
-            $nb=Nationalite::add($nationalite);
-            $message = "ajoutée";  
-        }else{
-            $nationalite->setLibelle($_POST['libelle']);
-            $nationalite->setNum($_POST['num']);
-            $nb=Nationalite::update($nationalite);
-            $message = "modifiée";  
-        }
+    case 'list';
+    //traitement du form de recherche
+    $libelle="";
+    $continentSel="Tous";
+    if(!empty($_POST['libelle']) || !empty($_POST['continentSel'])){
+        $libelle=$_POST['libelle'];
+        $continentSel=$_POST['continentSel'];
+    }
+    $lesContinents=Continent::findAll();
+    $lesNationalites=Nationalite::findAll($libelle,$continentSel);
+            include('vues/nationalite/listeNationalite.php');
+            break;
 
-        if($nb==1){
-            $_SESSION['message']=["success"=>"La nationalite a bien été $message"];
-        }else{
-            $_SESSION['message']=["danger"=>"La nationalite n'a pas été $message"];
-        }
-        header('location: index.php?uc=nationalites&action=list');
-        break;
+        case 'add';
+            $mode="Ajouter";
+            $lesContinents=Continent::findAll();
+            include('vues/nationalite/formNationalite.php');
+            break;
+
+        case 'update';
+            $mode="Modifier";
+            $lesContinents=Continent::findAll();
+            $laNationalite=Nationalite::findById($_GET['num']);
+            include('vues/nationalite/formNationalite.php');
+            break;
+
+        case 'delete';
+            $laNationalite=Nationalite::findById($_GET['num']);
+            $nb=Nationalite::delete($laNationalite);
+            if($nb==1){
+                $_SESSION['message']=["success"=>"Le nationalite a bien été supprimé"];
+            }else{
+                $_SESSION['message']=["danger"=>"Le nationalite n'a pas été supprimé"];
+            }
+            header('location:index.php?uc=nationalite&action=list');
+            break;
+
+        case 'validerForm';
+            $nationalite=new Nationalite();
+            $continent=Continent::findById($_POST['continent']);
+            $nationalite->setLibelle($_POST['libelle']) //2 setter
+                        ->setContinent($continent);
+            if(empty($_POST['num'])){//cas d'une création
+                $nb=Nationalite::add($nationalite);
+                $message ="ajouté"; 
+            }else{//cas d'une modif
+                $nb=Nationalite::update($nationalite);
+                $message ="modifié";
+            }
+            if($nb==1){
+                $_SESSION['message']=["success"=>"Le nationalite a bien été $message"];
+            }else{
+                $_SESSION['message']=["danger"=>"Le nationalite n'a pas été $message"];
+            }
+            header('location:index.php?uc=nationalites&action=list');
+            break;
 }
-
-
-
-?>
